d43b11ff40fd08e8cc3480343622407d035df7fd
Ankur Seth <iankurseth@gmail.com>
2024-08-27 21:59:33 +0530

    Updated placeholder content

--- react-app/src/App/pages/MainPage/MainPage.tsx
+++ react-app/src/App/pages/MainPage/MainPage.tsx
@@ -1,4 +1,3 @@
-import { RedIcon } from "redicons";
 import { Footer, Header } from "../../components";
 
 
@@ -9,12 +8,9 @@ export default function MainPage () {
 			<Header />
 
 			<main className="min-h-screen px-4 py-12">
-				<section className="">
-					<a href="https://github.com/iaseth/vite-react-ts-starter" className="block w-64 m-auto bg-zinc-800 text-white text-center px-8 py-6 rounded">
-						<RedIcon name="bs-foo" className="mx-auto" size={40} />
-						<h1 className="H4">Starter</h1>
-						<h4>vite-react-ts-starter</h4>
-					</a>
+				<section className="text-center">
+					<h1 className="H4">Photoreactor</h1>
+					<h4>Created By Ankur Seth</h4>
 				</section>
 			</main>
 
