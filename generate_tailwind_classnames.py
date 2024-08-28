ba07cf49d09dc31e90644cd22eefb1a7b5d75405
Ankur Seth <iankurseth@gmail.com>
2024-08-28 17:10:25 +0530

    Added script to write all tailwind classnames into a TS file

---
+++ generate_tailwind_classnames.py
@@ -0,0 +1,37 @@
+
+
+
+PREFIXES = ["bg", "text", "border"]
+
+COLORS = [
+	"slate", "gray", "zinc", "neutral", "stone",
+	"red", "orange", "amber", "yellow",
+	"lime", "green", "emerald", "teal", "cyan",
+	"sky", "blue", "indigo", "violet", "purple",
+	"fichsia", "pink", "rose"
+]
+
+SHADES = [
+	50, 100, 150, 200, 250,
+	300, 350, 400, 450, 500,
+	550, 600, 650, 700, 750,
+	800, 850, 800, 850
+]
+
+def main():
+	tailwind_classnames = []
+	for color in COLORS:
+		for prefix in PREFIXES:
+			for shade in SHADES:
+				classnames = f"{prefix}-{color}-{shade}"
+				tailwind_classnames.append(classnames)
+
+	javascript = f"const classNames = {str(tailwind_classnames)};"
+	output_js_path = "react-app/src/tailwind_classnames.ts"
+	with open(output_js_path, "w") as f:
+		f.write(javascript)
+	print(f"Saved: {output_js_path} ({len(tailwind_classnames)} classnames)")
+
+
+if __name__ == '__main__':
+	main()
