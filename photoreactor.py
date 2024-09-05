4009090d2b09903b6198cbf463f8b7431e9eedb7
Ankur Seth <iankurseth@gmail.com>
2024-09-05 12:05:26 +0530

    Now accepting script and screenshot_path as args

--- photoreactor.py
+++ photoreactor.py
@@ -1,5 +1,7 @@
 import json
+import os
 import shutil
+import sys
 import time
 
 from playwright.sync_api import Page, expect
@@ -14,7 +16,7 @@ def record_screencast(script_json, resolution, screenshot_file_path):
 	with sync_playwright() as playwright:
 		browser = playwright.chromium.launch(headless=True)
 		context = browser.new_context(viewport=resolution)
-		print("\tCreated browser context")
+		print(f"\tCreated a {resolution['width']}x{resolution['height']} browser context")
 
 		page = context.new_page()
 		t1 = time.time()
@@ -38,31 +40,44 @@ def record_screencast(script_json, resolution, screenshot_file_path):
 
 
 def main():
-	script = {
-		"title":"Example",
-		"height":"480px",
-		"width":"1080px",
-		"padding":"60px 100px",
-		"backgroundColor":"#dc2626",
-		"color":"#eee",
-		"fontFamily":"Ubuntu",
-		"fontWeight":"700",
-		"borderColor":"#ef4444",
-		"borderWidth":"10px",
-		"borderStyle":"solid",
-		"children":[
-			{"text":"Photoreactor","fontSize":"96px"},
-			{"text":"Taking Screenshots with Python","fontSize":"48px", "padding": "10px 0px"},
-			{"text":"Created by Ankur Seth", "fontSize":"32px", "padding": "60px 0px 0px 0px"}
-		]
-	}
+	args = sys.argv[1:]
+	if len(args) < 2:
+		print(f"Usage:\n\tpython3 photoreactor.py script.json screenshot.png")
+		return
 
-	script_json = json.dumps(script)
+	script_json_path = args[0]
+	screenshot_file_path = args[1]
+
+	if not script_json_path.endswith('.json'):
+		print(f"Script must be a JSON file: '{script_json_path}'")
+		return
+	elif not os.path.isfile(script_json_path):
+		print(f"Script not found: '{script_json_path}'")
+		return
+
+	with open(script_json_path) as f:
+		script_json = f.read()
+		script = json.loads(script_json)
+
+	if not screenshot_file_path.endswith('.png'):
+		print(f"Screenshot must be a PNG file: '{screenshot_file_path}'")
+		return
+	elif os.path.isfile(screenshot_file_path):
+		print(f"Screenshot exists: '{screenshot_file_path}'")
+		response = input(f"Do you want to overwrite? (y/n) ")
+		if response != "y": return
+
+	try:
+		height = int(script['height'][:-2])
+		width = int(script['width'][:-2])
+	except Exception as e:
+		height = 1080
+		width = 1080
+
 	resolution = {
-		'height': 480,
-		'width': 1080
+		'height': height,
+		'width': width
 	}
-	screenshot_file_path = "screenshot.png"
 
 	record_screencast(script_json, resolution, screenshot_file_path)
 
